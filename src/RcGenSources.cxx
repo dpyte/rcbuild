160db42d6e60800132666f709d8f3b09e7224f9a
Dhruv Patel <pateldrv456@gmail.com>
2021-07-10 20:48:04 -0400

    finsih implementing fancy IO

--- src/RcGenSources.cxx
+++ src/RcGenSources.cxx
@@ -1,9 +1,29 @@
 #include "RcGenSources.hxx"
 #include <iostream>
-#include <type_traits>
+#include <thread>
+#include <chrono>
+#include "RcError.hxx"
 
 using RcValueTable_Ptr = std::shared_ptr<RcValues>;
 
+enum TABLE_VALUES {
+    FETCH_SRC,
+    FETCH_TYPE,
+    GIT_DEPTH,
+    LOCATION,
+    PROJECT_NAME,
+};
+
+static constexpr const auto project_name_list = {
+    "project_1", "project_2", "project_3", "project_4",
+    "project_5", "project_6", "project_7", "project_8",
+    "project_9", "project_10", "project_11", "project_12",
+    "project_13", "project_14", "project_15", "project_16",
+    "project_17", "project_18", "project_19", "project_20",
+    "project_21", "project_22", "project_23", "project_24",
+    "project_25", "project_26", "project_27", "project_28",
+    "project_29", "project_30", "project_31", "project_32",
+};
 
 template <typename Value, typename AssertType = bool>
 static AssertType assert_extracted_value(Value val, const AssertType type) {
@@ -12,37 +32,81 @@ static AssertType assert_extracted_value(Value val, const AssertType type) {
 
 RCGenSources::RCGenSources(const std::string &fname) noexcept {
     rc_path_to_config = fname;
-    parse_file = toml::parse(fname);
+    parse_file = toml::parse<toml::discard_comments>(fname);
 }
 
 // !NOTE: Kinda redundant
 bool RCGenSources::status() const noexcept {
-
     std::ifstream ifs(rc_path_to_config, std::ios_base::binary);
     return ifs.good();
 }
 
-std::vector<RcValueTable_Ptr> RCGenSources::construct_table() {
+static auto check_for_project_names(const toml::value &tml) {
+    auto retval = 0;
+    for (auto iter : project_name_list) {
+        // Do not return a value if the project name is not in ascending order.
+        // Consider the case:
+        //      - project_1, project_3, ...
+        if (tml.contains(iter)) retval++;
+    }
+    return retval;
+}
 
+static std::vector<std::pair<TABLE_VALUES, bool>>
+rc_table_detect_values(const toml::value &tml) {
+    std::vector<std::pair<TABLE_VALUES, bool>> retval;
+
+    retval.emplace_back(std::make_pair(PROJECT_NAME, tml.contains("project_name")));
+    retval.emplace_back(std::make_pair(FETCH_SRC, tml.contains("fetch_src")));
+    retval.emplace_back(std::make_pair(FETCH_TYPE, tml.contains("fetch_type")));
+    retval.emplace_back(std::make_pair(LOCATION, tml.contains("location")));
+    retval.emplace_back(std::make_pair(GIT_DEPTH, tml.contains("git_depth")));
+
+    return retval;
+}
+
+static void print_packages(const std::vector<std::pair<TABLE_VALUES, bool>> &tb) {
+    for (const auto &iter: tb) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(128));
+        switch (iter.first) {
+            case LOCATION: RcError::rc_report_error(RcError::STATUS,
+                    "found location");
+                break;
+            case FETCH_SRC: RcError::rc_report_error(RcError::STATUS,
+                    "found fetch_src");
+                break;
+            case FETCH_TYPE: RcError::rc_report_error(RcError::STATUS,
+                    "found fetch_type");
+                break;
+            case GIT_DEPTH: RcError::rc_report_error(RcError::STATUS,
+                    "found git_depth");
+                break;
+            case PROJECT_NAME: RcError::rc_report_error(RcError::STATUS,
+                    "found project_name");
+                break;
+        }
+    }
+}
+
+std::vector<RcValueTable_Ptr> RCGenSources::construct_table() {
     const auto title = toml::find<std::string>(parse_file, "title");
-    const auto check_type = assert_extracted_value<bool>(title.empty(), true);
+    const auto check_type = assert_extracted_value<bool>(title.empty(), false);
     if (!check_type) {
-      std::fprintf(stderr,
-                   "err :: missing required label from the configuration file."
-                   // This statement makes no sense but oh well ...
-                   "\n'title' is required in order to parse the file\n");
-      exit(EXIT_FAILURE);
+        RcError::rc_report_error(RcError::FATAL,
+            "err :: detected empty string in title label\n"
+                "%s ensure to set the title string to the name of the project\n",
+            RcError::rc_error_space);
     }
 
     const auto projects = toml::find(parse_file, "projects");
-    for (auto iter = 0; iter != RCBUILD_MAX; iter++) {
-        const auto project_number = "project_" + std::to_string(iter);
-        const auto &project = toml::find(projects, project_number);
-        const auto rc_project_name = toml::find<std::string>(project, "project_name");
-        std::cerr << " project name >> " << rc_project_name << '\n';
-    }
+    const auto number_of_projects = check_for_project_names(projects);
+    RcError::rc_report_error(RcError::MESSAGE,
+        "amount of detected projects: %ld", number_of_projects);
 
+    // for (auto iter : project_name_list) {
+    for (auto iter = 0; iter < number_of_projects; iter++) {
+        const auto detected_values = rc_table_detect_values(projects);
+        print_packages(detected_values);
+    }
     return value_table;
 }
-
-
--- src/main.cxx
+++ src/main.cxx
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "Path.hxx"
-#include <toml.hpp>
+#include "RcError.hxx"
 #include "RcGenSources.hxx"
 
 int main(int argc, const char **argv) {
@@ -8,17 +8,18 @@ int main(int argc, const char **argv) {
     bool stage_one = false;
     std::string path_to_rcbuild;
     if (argc == 1) {
-        fprintf(stderr, "Scanning for RCBUILD file in current directory\n");
+        RcError::rc_report_error(RcError::MESSAGE, "Scanning for RCBUILD file in current directory\n");
         Path::set_path(path_to_rcbuild);
         path_to_rcbuild.append(Path::rcbuild_filename);
         stage_one = true;
     }
 
     if (stage_one) {
-        std::fprintf(stderr, "Attempting to read from the file '%s' ... ",
+        RcError::rc_report_error(RcError::MESSAGE, "Attempting to read from the file '%s' ... ",
             path_to_rcbuild.c_str());
         auto rc_gen =  RCGenSources(path_to_rcbuild);
-        std::fprintf(stderr, "%s\n", rc_gen.status() ? "success" : "failure");
+        RcError::rc_report_error(rc_gen.status() ? RcError::MESSAGE : RcError::FATAL,
+            "%s\n", rc_gen.status() ? "success!" : "failure");
         const auto t = rc_gen.construct_table();
     }
 
