a3053143b3b341e6f2c6c17fc4a1d61be20742c0
Dhruv Patel <pateldrv456@gmail.com>
2021-07-10 20:47:46 -0400

    finsih implementing fancy IO

--- src/RcError.cxx
+++ src/RcError.cxx
@@ -1,3 +1,80 @@
-//
-// Created by patel on 7/10/2021.
-//
+#include "RcError.hxx"
+#include <cstring>
+#include <cstdio>
+#include <stdexcept>
+#include <iostream>
+
+enum COLOR {
+    ERC_BLUE = 34,
+    ERC_DEFAULT = 39,
+    ERC_GREEN = 32,
+    ERC_RED = 31,
+    EBG_RED = 41,
+    EBG_BLUE = 44,
+    EBG_DEFAULT = 49
+};
+
+class RCErrPrint {
+private:
+    static constexpr const auto color_code = "\033[";
+    COLOR code;
+public:
+    explicit RCErrPrint(COLOR cl);
+    friend std::ostream &operator << (std::ostream & os, const RCErrPrint &rcerr);
+};
+
+RCErrPrint::RCErrPrint(COLOR cl) : code(cl) {}
+
+std::ostream &operator<<(std::ostream& os, const RCErrPrint& rcerr) {
+    return os << RCErrPrint::color_code << rcerr.code << 'm';
+}
+
+static const char *err_style(const RcError::rc_error_type &et) {
+    switch (et) {
+        case RcError::FATAL: return " [ERROR] ";
+        case RcError::REQUIRED: return " <REQUIRED> ";
+        case RcError::WARNING: return " !WARNING! ";
+        case RcError::STATUS: return " ~> ";
+        case RcError::MESSAGE: return " >> ";
+    }
+    return "";
+}
+
+static void concatenate_message(const RcError::rc_error_type &et, const std::string &str) {
+    switch (et) {
+        case RcError::FATAL:
+            std::cout << RCErrPrint(ERC_RED) << str << RCErrPrint(ERC_DEFAULT) << '\n';
+            throw std::exception();
+        case RcError::REQUIRED:
+            std::cout << RCErrPrint(ERC_GREEN) << str << RCErrPrint(ERC_DEFAULT) << '\n';
+            throw std::exception();
+        case RcError::WARNING:
+            std::cout << RCErrPrint(ERC_BLUE) << str << RCErrPrint(ERC_DEFAULT) << '\n';
+            break;
+        case RcError::STATUS:
+            std::cout << RCErrPrint(ERC_GREEN) << str << RCErrPrint(ERC_DEFAULT) << '\n';
+            break;
+        case RcError::MESSAGE:
+            std::cout << str << '\n';
+            break;
+    }
+}
+
+void RcError::rc_report_error(rc_error_type e_type, const char* fmt, ...) {
+
+    va_list args;
+    va_start(args, fmt);
+
+    const auto sz = std::strlen(fmt) * sizeof(fmt) * 64;
+    const auto str = new (std::nothrow) char[sz];
+    if (!str) {
+        std::fprintf(stderr, " system warning: <rc_report_error> -> allocation failure\n");
+        throw std::exception();
+    }
+    std::vsnprintf(str, sz, fmt, args);
+    va_end(args);
+
+    std::string report_string(str);
+    delete [] str;
+    concatenate_message(e_type, err_style(e_type)+ report_string);
+}
--- src/RcError.hxx
+++ src/RcError.hxx
@@ -1,8 +1,22 @@
-//
-// Created by patel on 7/10/2021.
-//
-
 #ifndef RCBUILD_RCERROR_HXX
 #define RCBUILD_RCERROR_HXX
 
-#endif //RCBUILD_RCERROR_HXX
+#include "Details.hxx"
+#include <cstdarg>
+
+namespace RcError {
+
+enum rc_error_type {
+    FATAL,
+    MESSAGE,
+    REQUIRED,
+    STATUS,
+    WARNING,
+};
+
+static constexpr const auto rc_error_space = '\t';
+
+RCEXTERN void rc_report_error(rc_error_type e_type, const char* fmt, ...);
+
+} // namespace RcError
+#endif // RCBUILD_RCERROR_HXX
