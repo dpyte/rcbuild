f33845d706c9ed3fbc43351e2539d630e08c6af0
Dhruv Patel <pateldrv456@gmail.com>
2021-07-15 21:01:43 -0400

    remove redundant code and use std::filesystem

--- src/Path.cxx
+++ src/Path.cxx
@@ -3,11 +3,5 @@
 #include <exception>
 
 void Path::set_path(std::string &rcbuild) {
-    char path[path_max];
-    if (getcwd(path, sizeof(path)) == nullptr) {
-        RcError::rc_report_error(RcError::FATAL,
-            "error <internal> :: getcwd() error \n");
-        std::terminate();
-    }
-    rcbuild = std::string(path);
+    rcbuild = std::filesystem::current_path().string();
 }
--- src/Path.hxx
+++ src/Path.hxx
@@ -1,28 +1,12 @@
 #ifndef RCBUILD_PATH_HXX
 #define RCBUILD_PATH_HXX
 
-#if defined(__WINDOWS__) || defined(_WIN64)
-#include <windows.h>
-#elif defined(__gnu_linux__) || defined(__linux__) || defined(__APPLE__)
-#include <unistd.h>
-#endif
-
-#include <climits>
-#include <cstddef>
-#include <cstring>
-#include <memory>
+#include <filesystem>
 #include <string>
 #include "Details.hxx"
 
 namespace Path {
     static constexpr auto rcbuild_filename = "/RCBUILD";
-    static constexpr auto path_max = PATH_MAX;
-
-    struct directory_path {
-        std::string curr_path;
-        std::string proposed_path;
-    };
-
     RCEXTERN void set_path(std::string& rcbuild);
 }
 #endif //RCBUILD_PATH_HXX
