3af00bd0b4c1087d1be164cc18209f8bc9c6ef59
Dhruv Patel <pateldrv456@gmail.com>
2021-07-15 21:01:57 -0400

    fix shared_ptr issue

--- src/main.cxx
+++ src/main.cxx
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "Path.hxx"
-#include "RcError.hxx"
-#include "RcGenSources.hxx"
+#include "RcAsyncInstall.hxx"
+
+using RcValuesVec_Ptr = std::vector<RcValues*>;
 
 int main(int argc, const char **argv) {
 
@@ -20,7 +21,11 @@ int main(int argc, const char **argv) {
         auto rc_gen =  RCGenSources(path_to_rcbuild);
         RcError::rc_report_error(rc_gen.status() ? RcError::MESSAGE : RcError::FATAL,
             "%s\n", rc_gen.status() ? "success!" : "failure");
-        const auto rc_gen_table = rc_gen.construct_table();
+        auto rc_gen_table = rc_gen.construct_table();
+        // Apparently we will have to declare this variable as such (and without using the auto keyword)
+        // as it will cause failure in code compilation
+        RcAsync::RcInstaller rc_install = RcAsync::RcInstaller(rc_gen_table);
+        const auto rc_installer_exit_code = rc_install.rc_async_installer();
     }
 
     return 0;
